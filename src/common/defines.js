990521ab0d5e1196c36b1e6100cb56b75359d015
yuri2peter <yuri2peter@qq.com>
2019-07-10 14:22:35 +0800

    use http

--- src/common/defines.js
+++ src/common/defines.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 export default {
   IS_PRODUCTION: isProduction,
-  CLOUD_HOME: 'https://cloud.ylui.yuri2.cn',
+  CLOUD_HOME: 'http://cloud.ylui.yuri2.cn',
   CLOUD_API: 'https://mongo.yuri2.cn/api/oss',
   COLOR: {
     THEME: '#6b96cf',
